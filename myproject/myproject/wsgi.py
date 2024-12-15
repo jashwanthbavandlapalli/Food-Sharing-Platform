5fc92aa40aa7cf8a2046c8f486cce36a7e648c79
Jashwanth Bavandlapalli <bavandlapallijashwanth@gmail.com>
2024-12-15 21:59:28 +0530

    committed render

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
+'''
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,10 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
-
+'''
+import os
+from django.core.wsgi import get_wsgi_application
 from waitress import serve
-from myproject.wsgi import application
 
+# Set the default Django settings module for the 'wsgi' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+# Get the WSGI application for the project
+application = get_wsgi_application()
+
+# Run Waitress to serve the application
 if __name__ == "__main__":
-    serve(application, host='0.0.0.0', port=8080)
+    serve(application, host='0.0.0.0', port=8000)
 
